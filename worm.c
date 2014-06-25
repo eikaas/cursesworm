fd823ae5fa4602f45a23efb75317493f4c206337
Robin Eikaas <robin@eikaas.no>
2014-06-25 17:35:22 +0200

    Restructured structs - adding linked list of links

--- worm.c
+++ worm.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <assert.h>
 
 #define DELAY 40000
 #define NUM_WORMS 20
@@ -11,15 +12,18 @@
 #define TRUE 1
 #define FALSE 0
 
-typedef struct worm {
+typedef struct body {
     int pos_x;
     int pos_y;
 
     int vel_x;
     int vel_y;
 
-    int length;
+    struct body *next;
+} *Body;
 
+typedef struct worm {
+    struct body *body;
     struct worm *next;
 } *Worm;
 
@@ -33,9 +37,6 @@ typedef struct apple {
 
 // Create a new worm
 Worm newWorm(int pos_x, int pos_y);
-
-// Get the next worm in the list
-Worm nextWorm(Worm currentWorm);
 
 // Update the worm
 void updateWorm(Worm currentWorm, int screen_height, int screen_width);
@@ -73,50 +74,86 @@ void drawApple(Apple currentApple) {
     mvprintw(currentApple->pos_y, currentApple->pos_x, "@");
 }
 
+// INCOMPLETE, doesnt grow!
+void growWorm(Worm currentWorm) {
+    int bodyCount = 0;
+
+    // Get the first body part
+    Body currentBodypart = currentWorm->body;
+
+    // Traverse the list untill we get to the last body part
+    while (currentBodypart != NULL) {
+        bodyCount++;
+        currentBodypart = currentBodypart->next;
+    }
+
+    // Insert new bodypart at the end of the worm
+    currentBodypart = malloc(sizeof(struct body));
+    currentBodypart->pos_x = currentWorm->body->pos_x;
+    currentBodypart->pos_y = currentWorm->body->pos_y;
+
+    // The velocity should be the same as the parent body part
+    // NB: Might run into issues here if we grow the worm near the edge of the screen
+    currentBodypart->vel_x = currentWorm->body->vel_y;
+    currentBodypart->vel_y = currentWorm->body->vel_y;
+
+    currentBodypart->next = NULL;
+}
+
 Worm newWorm(int pos_x, int pos_y) {
     Worm tmpWorm = malloc(sizeof(struct worm));
+    assert(tmpWorm != NULL);
 
-    if (tmpWorm) {
-        tmpWorm->pos_x = pos_x;
-        tmpWorm->pos_y = pos_y;
+    // Give the worm an initial body part
+    tmpWorm->body = malloc(sizeof(struct body));
+    assert(tmpWorm->body != NULL);
 
-        tmpWorm->vel_x = getRandomInt(FALSE, TRUE) ? 1 : -1;
-        tmpWorm->vel_y = getRandomInt(FALSE, TRUE) ? 1 : -1;
+    tmpWorm->body->pos_x = pos_x;
+    tmpWorm->body->pos_y = pos_y;
 
-        tmpWorm->length = 0;
+    // Set a random initial velocty
+    tmpWorm->body->vel_x = getRandomInt(FALSE, TRUE) ? -1 : 1;
+    tmpWorm->body->vel_y = getRandomInt(FALSE, TRUE) ? -1 : 1;
 
-        tmpWorm->next = NULL;
-    }
+    tmpWorm->body->next = NULL;
+
+    // This is the last worm
+    tmpWorm->next = NULL;
 
     return tmpWorm;
 }
 
-Worm nextWorm(Worm currentWorm) {
-    return currentWorm->next;
-}
-
 void updateWorm(Worm currentWorm, int screen_height, int screen_width) {
+    assert(currentWorm != NULL);
+    assert(currentWorm->body != NULL);
     // if x position is on either left or right side of the screen, flip vel_x
-    if (currentWorm->pos_x < 0 || currentWorm->pos_x > screen_width) {
-        currentWorm->vel_x *= -1;
+    if (currentWorm->body->pos_x < 0 || currentWorm->body->pos_x > screen_width) {
+        currentWorm->body->vel_x *= -1;
     }
 
     // if y position is on either top or bottom side of the screen, flip vel_y
-    if (currentWorm->pos_y < 0 || currentWorm->pos_y > screen_height) {
-        currentWorm->vel_y *= -1;
+    if (currentWorm->body->pos_y < 0 || currentWorm->body->pos_y > screen_height) {
+        currentWorm->body->vel_y *= -1;
     }
 
     // Update position
-    currentWorm->pos_x += currentWorm->vel_x;
-    currentWorm->pos_y += currentWorm->vel_y;
+    currentWorm->body->pos_x += currentWorm->body->vel_x;
+    currentWorm->body->pos_y += currentWorm->body->vel_y;
 }
 
 void drawWorm(Worm currentWorm) {
-    int i, offset_y, offset_x;
-    // Draw head
-    mvprintw(currentWorm->pos_y, currentWorm->pos_x, "o_o");
+    assert(currentWorm != NULL);
+    assert(currentWorm->body != NULL);
+
+    Body tmpBody = currentWorm->body;
+
+    do {
+        mvprintw(tmpBody->pos_y, tmpBody->pos_x, "O");
+        tmpBody = tmpBody->next;
+    } while (tmpBody != NULL);
 
     // Draw body
+    /*
     for (i = 1; i < currentWorm->length + 1; i++) {
         offset_y = currentWorm->vel_y * i * -1;
         offset_x = currentWorm->vel_x * i * -1;
@@ -131,6 +168,7 @@ void drawWorm(Worm currentWorm) {
     offset_y = offset_y > 0 ? offset_y + 1 : offset_y - 1;
     offset_x = offset_x > 0 ? offset_x + 1 : offset_x - 1;
     mvprintw(currentWorm->pos_y + offset_y, currentWorm->pos_x + offset_x, " . ");
+    */
 }
 
 int getRandomInt(int from, int to) {
@@ -142,11 +180,13 @@ int getRandomInt(int from, int to) {
 }
 
 int checkCollision(Worm worm, Apple apple) {
+    assert(worm != NULL);
+    assert(worm->body != NULL);
+    assert(apple != NULL);
     int retval = FALSE;
 
-    // Doesnt work
-    int deltaX = abs(worm->pos_x - apple->pos_x);
-    int deltaY = abs(worm->pos_y - apple->pos_y);
+    int deltaX = abs(worm->body->pos_x - apple->pos_x);
+    int deltaY = abs(worm->body->pos_y - apple->pos_y);
 
     if (deltaX <= 2 && deltaY <= 1) {
         retval = TRUE;
@@ -200,14 +240,21 @@ int main(int argc, const char *argv[]) {
         attron(COLOR_PAIR(2));
         while (currentWorm != NULL) {
             updateWorm(currentWorm, screen_height, screen_width);
+
+            /*
+             * Post-Bug note: Dont forget to make a copy when traversing a list!
+             * after drawing drawWorm(), currentWorm->body was a null pointer, resulting in the
+             * following assertion to faile. (Well, after segfaulting in checkCollision()).
+             */
             drawWorm(currentWorm);
+            assert(currentWorm->body != NULL);
 
             currentApple = headApple;
+
             while (currentApple != NULL) {
                 if (checkCollision(currentWorm, currentApple)) {
                     currentApple->pos_x = getRandomInt(0, screen_width);
                     currentApple->pos_y = getRandomInt(0, screen_height);
-                    currentWorm->length++;
 
                 }
                 currentApple = currentApple->next;
